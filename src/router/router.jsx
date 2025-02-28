0d6e76bb493df0bcd18cf58c296d6edfcb2ad9b6
Adetoye daniel kehinde <150145750+DanXtech@users.noreply.github.com>
2025-02-28 12:05:28 +0100

    Update router.jsx

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home";
 import RegistrationForm from "../Pages/RegistrationForm";
+import Recommendations from "../Pages/Recommendations";
 import Dashboard from "../Pages/Dashboard";
 
 const router = createBrowserRouter([
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <Home /> },  // ✅ Home inside App
       { path: "registration", element: <RegistrationForm /> },  // ✅ Nested under "/"
+      { path: "recommendations", element: <Recommendations /> },  // ✅ Nested under "/"
       { path: "dashboard", element: <Dashboard /> },  // ✅ Nested under "/"
     ],
   },
